6b744fd5b63368280186ea0a00c95f5591f65a39
nwemo8373-prog <nwemo8373@gmail.com>
2026-09-08 23:54:48 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+const STELLA_CONFIG = {
+
+    SUPABASE_URL:
+        "https://lgwtpqcvfefgdnbwlbys.supabase.co",
+
+    SUPABASE_PUBLISHABLE_KEY:
+        "sb_publishable_XNeRaTGje_0mo_1G8lbtmw_EY-JbLoP",
+
+    HF_TOKEN:
+        "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",
+
+    HF_MODEL:
+        "Qwen/Qwen2.5-7B-Instruct",
+
+    HF_PROVIDER:
+        "auto"
+
+};
